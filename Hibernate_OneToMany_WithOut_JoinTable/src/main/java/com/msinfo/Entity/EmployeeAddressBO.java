3e13fab53a555c06dadeb81b4085b94ace1c4f83
Achu <Achu@DESKTOP-8O0H9G3.fios-router.home>
2019-01-05 01:59:36 -0500

    hibernate

---
+++ Hibernate_OneToMany_WithOut_JoinTable/src/main/java/com/msinfo/Entity/EmployeeAddressBO.java
@@ -0,0 +1,93 @@
+package com.msinfo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEE_ADDRESS")
+public class EmployeeAddressBO {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="EMPLOYEE_ADDR_ID")
+	private Integer employeeAddressId;
+	
+	@Column(name="STREET")
+	private String street;
+	
+	@Column(name="CITY")
+	private String city;
+	
+	@Column(name="STATE")
+	private String state;
+	
+	@Column(name="PINCODE")
+	private Integer pincode;
+	
+	@ManyToOne()
+	@JoinColumn(name="EMPLOYEE_ID")
+	private EmployeeBO employee;
+
+	public Integer getEmployeeAddressId() {
+		return employeeAddressId;
+	}
+
+	public void setEmployeeAddressId(Integer employeeAddressId) {
+		this.employeeAddressId = employeeAddressId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(Integer pincode) {
+		this.pincode = pincode;
+	}
+
+	public EmployeeBO getEmployee() {
+		return employee;
+	}
+	
+	public void setEmployee(EmployeeBO employee) {
+		this.employee = employee;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeAddressBO [employeeAddressId=" + employeeAddressId + ", street=" + street + ", city=" + city
+				+ ", state=" + state + ", pincode=" + pincode + ", employee=" + employee + "]";
+	}
+	
+}
+
+
---
+++ Hibernate_OneToMany_With_JoinTable/src/main/java/com/msinfo/client/TestClient.java
@@ -0,0 +1,54 @@
+package com.msinfo.client;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.msinfo.Entity.EmployeeAddressBO;
+import com.msinfo.Entity.EmployeeBO;
+import com.msinfo.Utils.HibernateUtil;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+			// Create table and insert value
+			session.beginTransaction();
+
+			EmployeeBO employee = getEmployee();
+			int empId = (int) session.save(employee);
+
+			session.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static EmployeeBO getEmployee() {
+		EmployeeBO emp = new EmployeeBO();
+		emp.setEmployeeId(100);
+		emp.setEmployeeName("Ashwin");
+		emp.setEmployeeDob(new Date());
+
+		EmployeeAddressBO address1 = new EmployeeAddressBO();
+		address1.setStreet("121 Main Street");
+		address1.setCity("Boston");
+		address1.setState("MA");
+		address1.setPincode(02150);
+
+		EmployeeAddressBO address2 = new EmployeeAddressBO();
+		address2.setStreet("122 Main Street");
+		address2.setCity("cambridge");
+		address2.setState("MA");
+		address2.setPincode(02151);
+		
+		emp.getEmployerAddress().add(address1);
+		emp.getEmployerAddress().add(address2);
+		
+		return emp;
+	}
+}

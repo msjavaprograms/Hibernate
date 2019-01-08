fda93e382c9382e62f7662d85d23de0ad3b3736c
Achu <Achu@DESKTOP-8O0H9G3.fios-router.home>
2019-01-07 22:44:03 -0500

    CRUD operations

---
+++ Hibernate5CRUDExamples/src/main/java/com/msinfo/Entity/EmployeeBO.java
@@ -0,0 +1,77 @@
+package com.msinfo.Entity;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name="EMPLOYEE")
+@DynamicUpdate
+public class EmployeeBO {
+
+	@Id
+	@Column(name="EMPLOYEE_ID",length=10)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer employeeId;
+	
+	@Column(name="NAME",length=20,nullable=false)
+	String employeeName;
+	
+	@Column(name="DOB",nullable=true)
+	Date employeeDob;
+	
+	@Column(name="EMPLOYEE_EMAIL",nullable=false)
+	private String employeeEmail;
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Date getEmployeeDob() {
+		return employeeDob;
+	}
+
+	public void setEmployeeDob(Date employeeDob) {
+		this.employeeDob = employeeDob;
+	}
+
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeBO [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeDob="
+				+ employeeDob + ", employeeEmail=" + employeeEmail + "]";
+	}
+
+	
+	
+}
---
+++ Hibernate5CRUDExamples/src/main/java/com/msinfo/client/TestClient.java
@@ -0,0 +1,109 @@
+package com.msinfo.client;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.msinfo.Entity.EmployeeBO;
+import com.msinfo.Utils.HibernateUtil;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+			// Create Record using Hibernate - save method
+			session.beginTransaction();
+			EmployeeBO emp1 = getEmployee1();
+			session.save(emp1);
+			session.getTransaction().commit();
+
+			// Create Record using Hibernate - persist method
+			session.beginTransaction();
+			EmployeeBO emp2 = getEmployee2();
+			session.save(emp2);
+			session.getTransaction().commit();
+
+			// Create Record using Hibernate - persist method
+			session.beginTransaction();
+			EmployeeBO emp3 = getEmployee3();
+			session.save(emp3);
+			session.getTransaction().commit();
+
+			// Read record using Hibernate - get method
+			EmployeeBO employee1 = session.get(EmployeeBO.class, 1);
+			System.out.println("Employee one Details: " + employee1);
+
+			// Read record using Hibernate - load method
+			EmployeeBO employee2 = session.load(EmployeeBO.class, 2);
+			System.out.println("Employee two Details: " + employee2);
+
+			// update table using Hibernate - update method
+			session.beginTransaction();
+			employee1.setEmployeeEmail("Achu@gmail.com");
+			session.update(employee1);
+			session.getTransaction().commit();
+
+			// update table using Hibernate - saveOrUpdate method
+			session.beginTransaction();
+			employee2.setEmployeeEmail("Musa@gmail.com");
+			session.saveOrUpdate(employee2);
+			session.getTransaction().commit();
+
+			// update table using Hibernate - saveOrUpdate method
+			session.beginTransaction();
+			employee2.setEmployeeName("MuthuSaran");
+			session.merge(employee2);
+			session.getTransaction().commit();
+
+			// Delete employee
+			if (emp3 != null) {
+				session.beginTransaction();
+				session.delete(emp3);
+				session.getTransaction().commit();
+			}
+
+			// Read record using Hibernate - get method
+			EmployeeBO empl1 = session.get(EmployeeBO.class, 1);
+			System.out.println("Employee one Details: " + empl1);
+
+			// Read record using Hibernate - load method
+			EmployeeBO empl2 = session.load(EmployeeBO.class, 2);
+			System.out.println("Employee two Details: " + empl2);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static EmployeeBO getEmployee1() {
+		EmployeeBO emp = new EmployeeBO();
+		emp.setEmployeeId(100);
+		emp.setEmployeeName("Ashwin");
+		emp.setEmployeeDob(new Date());
+		emp.setEmployeeEmail("Ashwin@gmail.com");
+
+		return emp;
+	}
+
+	private static EmployeeBO getEmployee2() {
+		EmployeeBO emp = new EmployeeBO();
+		emp.setEmployeeId(101);
+		emp.setEmployeeName("Muthu");
+		emp.setEmployeeDob(new Date());
+		emp.setEmployeeEmail("Saran@gmail.com");
+
+		return emp;
+	}
+
+	private static EmployeeBO getEmployee3() {
+		EmployeeBO emp = new EmployeeBO();
+		emp.setEmployeeId(103);
+		emp.setEmployeeName("Agnisha");
+		emp.setEmployeeDob(new Date());
+		emp.setEmployeeEmail("Agnisha@gmail.com");
+		return emp;
+	}
+}
